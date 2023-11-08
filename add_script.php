cb799ee36b8bee005ceff86a22a3bb8b0d257538
Erodonx <stalcha01@gmail.com>
2023-11-08 14:30:23 +0100

    test

--- add_script.php
+++ add_script.php
@@ -7,13 +7,13 @@ try {
             $conn->beginTransaction();
             // Ajouter une nouvelle catégorie
             $stmt = $conn->prepare("INSERT INTO disc (disc_title, disc_year, disc_picture,disc_genre,disc_label,artist_id,disc_price) VALUES (:disc_title, :disc_year,:disc_picture, :disc_genre,:disc_label,:artist_id,:disc_price)");
-            $stmt->bindValue(':disc_title', $_REQUEST['title']);
-            $stmt->bindValue(':disc_year', $_REQUEST['year']);
-            $stmt->bindValue(':disc_picture', $_REQUEST['picture']);
-            $stmt->bindValue(':disc_genre', $_REQUEST['genre']);
-            $stmt->bindValue(':disc_label', $_REQUEST['label']);
-            $stmt->bindValue(':artist_id', $_REQUEST['artist']);
-            $stmt->bindValue(':disc_price', $_REQUEST['price']);
+            $stmt->bindValue(':disc_title', $_POST['title']);
+            $stmt->bindValue(':disc_year', $_POST['year']);
+            $stmt->bindValue(':disc_picture', $_POST['picture']);
+            $stmt->bindValue(':disc_genre', $_POST['genre']);
+            $stmt->bindValue(':disc_label', $_POST['label']);
+            $stmt->bindValue(':artist_id', $_POST['artist']);
+            $stmt->bindValue(':disc_price', $_POST['price']);
             $stmt->execute();
             //$id_categorie = $conn->lastInsertId();
 
--- update_script.php
+++ update_script.php
@@ -1,8 +1,9 @@
 <?php
 include 'pdo_login.php';
 $stmt2 = $conn->query("SELECT * FROM disc");
-$vari=$_REQUEST['secret'];
-if ($_REQUEST['picture']=='')
+$vari=$_POST['secret'];
+var_dump($picture['name']);
+if ($_POST['picture']=='')
 {
     $stmt1 = $conn->query("SELECT * from disc");
     while ($row = $stmt1->fetch())
@@ -10,7 +11,7 @@ if ($_REQUEST['picture']=='')
      if ($row['disc_id']==$vari)
      {
 
-     $_REQUEST['picture']=$row['disc_picture'];
+     $_POST['picture']=$row['disc_picture'];
              
     }
     }
@@ -21,17 +22,17 @@ try {
             $conn->beginTransaction();
             // Ajouter une nouvelle catégorie
             $stmt = $conn->prepare("UPDATE disc SET disc_title=:title, disc_year=:yyear, disc_genre=:genre, disc_price=:price, artist_id=:artist, disc_picture=:picture   WHERE disc_id=:disc_id ");
-            $stmt->bindParam(':disc_id',$_REQUEST['secret']);
-            $stmt->bindParam(':title', $_REQUEST['title']);
-            $stmt->bindParam(':yyear',$_REQUEST['year']);
-            $stmt->bindParam(':genre',$_REQUEST['genre']);
-            $stmt->bindParam(':price',$_REQUEST['price']);
-            $stmt->bindParam(':picture',$_REQUEST['picture']);
-            $stmt->bindParam(':artist',$_REQUEST['artist']);
+            $stmt->bindParam(':disc_id',$_POST['secret']);
+            $stmt->bindParam(':title', $_POST['title']);
+            $stmt->bindParam(':yyear',$_POST['year']);
+            $stmt->bindParam(':genre',$_POST['genre']);
+            $stmt->bindParam(':price',$_POST['price']);
+            $stmt->bindParam(':picture',$_POST['picture']);
+            $stmt->bindParam(':artist',$_POST['artist']);
             $stmt->execute();
             $conn->commit();
             echo 'Modification dans la base de donnée effectué ! :)';
-            header('Refresh: 5;URL=php-crud.php');
+            header('Refresh: 15;URL=php-crud.php');
         } catch (PDOException $e) {
             // En cas d'erreur, annuler la transaction
             $conn->rollback();
